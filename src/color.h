e33fa8ec1589e8d56b2b5c3649d0b71b7eb9cae5
McSwaggens <McSwaggens@protonmail.com>
2016-09-20 14:32:37 +1000

    Added color.h

---
+++ src/color.h
@@ -0,0 +1,38 @@
+#ifndef __COLOR_H_
+#define __COLOR_H_
+
+struct Color
+{
+	int r;
+	int g;
+	int b;
+	int a;
+	
+	Color ()
+	{
+		this->r = 0;
+		this->g = 0;
+		this->b = 0;
+		this->a = 255;
+	}
+	
+	Color (int r, int g, int b)
+	{
+		this->r = r;
+		this->g = g;
+		this->b = b;
+		this->a = 255;
+	}
+	
+	Color (int r, int g, int b, int a)
+	{
+		this->r = r;
+		this->g = g;
+		this->b = b;
+		this->a = a;
+	}
+	
+	//TODO: Add operator overloads
+};
+
+#endif
