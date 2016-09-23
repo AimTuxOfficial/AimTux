3c3021eb487425c4b6e2f89bf22c33c100e1e7b0
Grep2002 <Jasminemarieamber@gmail.com>
2016-09-22 23:24:24 -0400

    Minor Minor Progress

--- src/SDK.h
+++ src/SDK.h
@@ -400,6 +400,12 @@ public:
 		typedef void(* oSetViewAngles)(void*, QAngle&);
 		return getvfunc<oSetViewAngles>(this, 19)(this, angle);
 	}
+
+	void ExecuteClientCmd(const char* pCmd) 
+	{
+		typedef void(* oCmd)(void*, const char*);
+		return getvfunc<oCmd>(this, 108)(this, pCmd);
+	}
 	
 };
 
@@ -410,6 +416,27 @@ public:
 		return GetVirtualFunction<int(*)(void*, const char*)>(this, 3)(this, Filename);
 	}
 };
+
+class CUserCmd
+{
+public:
+	virtual ~CUserCmd(){};
+	int command_number;
+	int tick_count;
+	Vector viewangles;
+	float forwardmove;
+	float sidemove;	
+	float upmove;
+	int buttons;
+	uint8_t impulse;
+	int weaponselect;
+	int weaponsubtype;
+	int random_seed;
+	short mousedx;
+	short mousedy;
+	bool hasbeenpredicted;
+};
+
 
 class CBaseEntity
 {
@@ -470,3 +497,42 @@ class IClientEntityList {
 		return getvfunc<oGetClientEntity>(this, 3)(this, index);
 	}
 };
+
+#define	FL_ONGROUND				(1<<0)	// At rest / on the ground
+#define FL_DUCKING				(1<<1)	// Player flag -- Player is fully crouched
+#define	FL_WATERJUMP			(1<<2)	// player jumping out of water
+#define FL_ONTRAIN				(1<<3) // Player is _controlling_ a train, so movement commands should be ignored on client during prediction.
+#define FL_INRAIN				(1<<4)	// Indicates the entity is standing in rain
+#define FL_FROZEN				(1<<5) // Player is frozen for 3rd person camera
+#define FL_ATCONTROLS			(1<<6) // Player can't move, but keeps key inputs for controlling another entity
+#define	FL_CLIENT				(1<<7)	// Is a player
+#define FL_FAKECLIENT			(1<<8)	// Fake client, simulated server side; don't send network messages to them
+// NON-PLAYER SPECIFIC (i.e., not used by GameMovement or the client .dll ) -- Can still be applied to players, though
+#define	FL_INWATER				(1<<9)	// In water
+#define IN_ATTACK		(1 << 0)
+#define IN_JUMP			(1 << 1)
+#define IN_DUCK			(1 << 2)
+#define IN_FORWARD		(1 << 3)
+#define IN_BACK			(1 << 4)
+#define IN_USE			(1 << 5)
+#define IN_CANCEL		(1 << 6)
+#define IN_LEFT			(1 << 7)
+#define IN_RIGHT		(1 << 8)
+#define IN_MOVELEFT		(1 << 9)
+#define IN_MOVERIGHT	(1 << 10)
+#define IN_ATTACK2		(1 << 11)
+#define IN_RUN			(1 << 12)
+#define IN_RELOAD		(1 << 13)
+#define IN_ALT1			(1 << 14)
+#define IN_ALT2			(1 << 15)
+#define IN_SCORE		(1 << 16)   
+#define IN_SPEED		(1 << 17)	
+#define IN_WALK			(1 << 18)	
+#define IN_ZOOM			(1 << 19)	
+#define IN_WEAPON1		(1 << 20)	
+#define IN_WEAPON2		(1 << 21)	
+#define IN_BULLRUSH		(1 << 22)
+#define IN_GRENADE1		(1 << 23)	
+#define IN_GRENADE2		(1 << 24)	
+#define	IN_ATTACK3		(1 << 25)
+
