ffd1990e32942875d0063888adaff0671d17e923
McSwaggens <McSwaggens@protonmail.com>
2016-09-24 16:20:42 +1000

    Added basic settings

---
+++ src/settings.h
@@ -0,0 +1,16 @@
+#ifndef __SETTINGS_H_
+#define __SETTINGS_H_
+namespace Settings
+{
+	namespace Aimbot
+	{
+		extern bool enabled;
+	}
+	
+	namespace ESP
+	{
+		extern bool enabled;
+	}
+}
+
+#endif
