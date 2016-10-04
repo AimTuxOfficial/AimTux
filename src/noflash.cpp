0a444e88a22a4a32177fafc7124cbb609e3eff53
LuK1337 <priv.luk@gmail.com>
2016-10-04 23:59:43 +0200

    Add stage check for NoFlash

--- src/noflash.cpp
+++ src/noflash.cpp
@@ -8,6 +8,9 @@ void Noflash::FrameStageNotify(ClientFrameStage_t stage)
 	if (!Settings::Noflash::enabled)
 		return;
 
+	if (stage != ClientFrameStage_t::FRAME_NET_UPDATE_POSTDATAUPDATE_START)
+		return;
+
 	C_BasePlayer* localPlayer = (C_BasePlayer*)entitylist->GetClientEntity(engine->GetLocalPlayer());
 	if (!localPlayer)
 		return;
