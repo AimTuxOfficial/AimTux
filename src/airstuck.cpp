ef3419995322f5fb062a6141b6a01b96d5333728
LuK1337 <priv.luk@gmail.com>
2016-10-07 15:34:33 +0200

    Don't allow to shoot when using airstuck

--- src/airstuck.cpp
+++ src/airstuck.cpp
@@ -7,6 +7,9 @@ void Airstuck::CreateMove(CUserCmd* cmd)
 	if (!Settings::Airstuck::enabled)
 		return;
 
+	if (cmd->buttons & IN_ATTACK)
+		return;
+
 	if (input->IsButtonDown(ButtonCode_t::KEY_F))
 		cmd->tick_count = INT_MAX;
 }
