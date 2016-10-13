dc57a8092ac9ec848eb55d7b04545b121a2922a4
McSwaggens <McSwaggens@protonmail.com>
2016-10-13 18:51:43 +1100

    Changed ts_spin_x and ts_spin_y text

--- src/WINDOWS/hvh_window.cpp
+++ src/WINDOWS/hvh_window.cpp
@@ -5,10 +5,10 @@ HVHWindow* hvh_window = nullptr;
 HVHWindow::HVHWindow (std::string title, Vector2D size, Vector2D position, Color backgroundColor)
 	: Window::Window (title, size, position, backgroundColor)
 {
-	ts_spin_x = new ToggleSwitch ("spin x", LOC (10, 10), 35, &Settings::AntiAim::enabled_X);
+	ts_spin_x = new ToggleSwitch ("x axis", LOC (10, 10), 35, &Settings::AntiAim::enabled_X);
 	AddComponent (ts_spin_x);
 	
-	ts_spin_y = new ToggleSwitch ("spin y", LOC (10, ts_spin_x->position.y + ts_spin_x->size.y + 10), 35, &Settings::AntiAim::enabled_Y);
+	ts_spin_y = new ToggleSwitch ("y axis", LOC (10, ts_spin_x->position.y + ts_spin_x->size.y + 10), 35, &Settings::AntiAim::enabled_Y);
 	AddComponent (ts_spin_y);
 	Hide ();
 }
