24daafa0ebc317ef8e09567f96b6c7ef4b548129
LuK1337 <priv.luk@gmail.com>
2016-10-14 10:20:56 +0200

    Unbreak misc window

--- src/WINDOWS/misc_window.cpp
+++ src/WINDOWS/misc_window.cpp
@@ -14,7 +14,7 @@ MiscWindow::MiscWindow (std::string title, Vector2D size, Vector2D position, Col
 	ts_spammer = new ToggleSwitch ("spammer", LOC (10, ts_autostrafe->position.y + ts_autostrafe->size.y + 10), 35, &Settings::Spammer::enabled);
 	AddComponent (ts_spammer);
 
-	ts_airstuck = new ToggleSwitch ("airstuck", LOC (10, ts_autostrafe->position.y + ts_autostrafe->size.y + 10), 35, &Settings::Airstuck::enabled);
+	ts_airstuck = new ToggleSwitch ("airstuck", LOC (10, ts_spammer->position.y + ts_spammer->size.y + 10), 35, &Settings::Airstuck::enabled);
 	AddComponent (ts_airstuck);
 
 	ts_showranks = new ToggleSwitch ("show ranks", LOC (10, ts_airstuck->position.y + ts_airstuck->size.y + 10), 35, &Settings::ShowRanks::enabled);
