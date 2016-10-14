8e6dd58f93d0b4096766d16f7dcd5c16e546184f
McSwaggens <McSwaggens@protonmail.com>
2016-10-14 15:29:06 +1100

    Changed window launch buttons to ToggleButtons

--- src/WINDOWS/main_window.cpp
+++ src/WINDOWS/main_window.cpp
@@ -5,26 +5,18 @@ MainWindow* main_window = nullptr;
 MainWindow::MainWindow (std::string title, Vector2D size, Vector2D position, Color backgroundColor)
 	: Window::Window (title, size, position, backgroundColor)
 {
-	aimbot_button = new OutlinedButton ("AimBot", LOC (10, 10), LOC (size.x - 20, 40));
+	aimbot_button = new ToggleButton ("AimBot", LOC (10, 10), LOC (size.x - 20, 40), &aimbot_window->shown);
 	AddComponent (aimbot_button);
 	
-	aimbot_button->OnClickedEvent = MFUNC (&MainWindow::aimbot_button_clicked, this);
-	
-	esp_button = new OutlinedButton ("Visuals", LOC (10, aimbot_button->position.y + aimbot_button->size.y + 10), LOC (size.x - 20, 40));
+	esp_button = new ToggleButton ("Visuals", LOC (10, aimbot_button->position.y + aimbot_button->size.y + 10), LOC (size.x - 20, 40), &esp_window->shown);
 	AddComponent (esp_button);
 	
-	esp_button->OnClickedEvent = MFUNC (&MainWindow::esp_button_clicked, this);
-	
-	hvh_button = new OutlinedButton ("H V H", LOC (10, esp_button->position.y + esp_button->size.y + 10), LOC (size.x - 20, 40));
+	hvh_button = new ToggleButton ("H V H", LOC (10, esp_button->position.y + esp_button->size.y + 10), LOC (size.x - 20, 40), &hvh_window->shown);
 	AddComponent (hvh_button);
 	
-	hvh_button->OnClickedEvent = MFUNC (&MainWindow::hvh_button_clicked, this);
-	
-	misc_button = new OutlinedButton ("Miscellaneous", LOC (10, hvh_button->position.y + hvh_button->size.y + 10), LOC (size.x - 20, 40));
+	misc_button = new ToggleButton ("Miscellaneous", LOC (10, hvh_button->position.y + hvh_button->size.y + 10), LOC (size.x - 20, 40), &misc_window->shown);
 	AddComponent (misc_button);
 	
-	misc_button->OnClickedEvent = MFUNC (&MainWindow::misc_button_clicked, this);
-
 	load1_button = new OutlinedButton ("Load #1", LOC (10, misc_button->position.y + misc_button->size.y + 10), LOC ((size.x - 20) / 2 - 5, 40));
 	AddComponent (load1_button);
 
@@ -46,26 +38,6 @@ MainWindow::MainWindow (std::string title, Vector2D size, Vector2D position, Col
 	save2_button->OnClickedEvent = MFUNC (&MainWindow::save2_button_clicked, this);
 
 	Show ();
-}
-
-void MainWindow::aimbot_button_clicked ()
-{
-	aimbot_window->Toggle ();
-}
-
-void MainWindow::esp_button_clicked ()
-{
-	esp_window->Toggle ();
-}
-
-void MainWindow::misc_button_clicked ()
-{
-	misc_window->Toggle ();
-}
-
-void MainWindow::hvh_button_clicked ()
-{
-	hvh_window->Toggle ();
 }
 
 void MainWindow::load1_button_clicked ()
--- src/WINDOWS/main_window.h
+++ src/WINDOWS/main_window.h
@@ -10,19 +10,15 @@ class MainWindow;
 class MainWindow : public Window
 {
 private:
-	Button* aimbot_button;
-	Button* esp_button;
-	Button* hvh_button;
-	Button* misc_button;
+	ToggleButton* aimbot_button;
+	ToggleButton* esp_button;
+	ToggleButton* hvh_button;
+	ToggleButton* misc_button;
 	Button* load1_button;
 	Button* load2_button;
 	Button* save1_button;
 	Button* save2_button;
 
-	void aimbot_button_clicked ();
-	void esp_button_clicked ();
-	void hvh_button_clicked ();
-	void misc_button_clicked ();
 	void load1_button_clicked ();
 	void load2_button_clicked ();
 	void save1_button_clicked ();
