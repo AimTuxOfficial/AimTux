149a06c5efc96698340fa1e61e75f0fad55379fa
McSwaggens <McSwaggens@protonmail.com>
2016-10-14 22:17:36 +1100

    Added BELOW and STACK macros

--- src/UI/stdui.h
+++ src/UI/stdui.h
@@ -6,3 +6,6 @@
 #include "slider.h"
 #include "listbox.h"
 #include "banner.h"
+
+#define BELOW(c) LOC (10, c->position.y + c->size.y + 10)
+#define STACK(c) LOC (c->position.x + c->size.x + 10, c->position.y)
