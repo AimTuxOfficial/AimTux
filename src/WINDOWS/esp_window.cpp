c63cb24ac78fec86835144a756311ea10c0ed801
McSwaggens <McSwaggens@protonmail.com>
2016-10-14 14:25:49 +1100

    Updated esp window

--- src/WINDOWS/esp_window.cpp
+++ src/WINDOWS/esp_window.cpp
@@ -8,10 +8,10 @@ ESPWindow::ESPWindow (std::string title, Vector2D size, Vector2D position, Color
 	ts_esp_enabled = new ToggleSwitch ("esp", LOC (10, 10), 35, &Settings::ESP::enabled);
 	AddComponent (ts_esp_enabled);
 	
-	ts_visibility_check = new ToggleSwitch ("visibility check", LOC (10, ts_esp_enabled->position.y + ts_esp_enabled->size.y + 10), 35, &Settings::ESP::visibility_check);
-	AddComponent (ts_visibility_check);
+	ba_player_info = new Banner ("Player Visuals", LOC (10, ts_esp_enabled->position.y + ts_esp_enabled->size.y + 10), size.x - 20);
+	AddComponent (ba_player_info);
 	
-	ts_walls = new ToggleSwitch ("walls", LOC (10, ts_visibility_check->position.y + ts_visibility_check->size.y + 10), 35, &Settings::ESP::Walls::enabled);
+	ts_walls = new ToggleSwitch ("walls", LOC (10, ba_player_info->position.y + ba_player_info->size.y + 10), 35, &Settings::ESP::Walls::enabled);
 	AddComponent (ts_walls);
 	
 	ts_tracer = new ToggleSwitch ("tracer", LOC (10, ts_walls->position.y + ts_walls->size.y + 10), 35, &Settings::ESP::Tracer::enabled);
@@ -26,19 +26,31 @@ ESPWindow::ESPWindow (std::string title, Vector2D size, Vector2D position, Color
 	ts_bones = new ToggleSwitch ("show bones", LOC (10, ts_health->position.y + ts_health->size.y + 10), 35, &Settings::ESP::Bones::enabled);
 	AddComponent (ts_bones);
 	
-	ts_chams_players = new ToggleSwitch ("player chams", LOC (10, ts_bones->position.y + ts_bones->size.y + 10), 35, &Settings::ESP::Chams::players);
+	ts_visibility_check = new ToggleSwitch ("visibility check", LOC (10, ts_bones->position.y + ts_bones->size.y + 10), 35, &Settings::ESP::visibility_check);
+	AddComponent (ts_visibility_check);
+	
+	ba_chams = new Banner ("Chams", LOC (10, ts_visibility_check->position.y + ts_visibility_check->size.y + 10), size.x - 20);
+	AddComponent (ba_chams);
+	
+	ts_chams_players = new ToggleSwitch ("players", LOC (10, ba_chams->position.y + ba_chams->size.y + 10), 35, &Settings::ESP::Chams::players);
 	AddComponent (ts_chams_players);
 	
-	ts_chams_arms = new ToggleSwitch ("cham arms", LOC (10, ts_chams_players->position.y + ts_chams_players->size.y + 10), 35, &Settings::ESP::Chams::arms);
+	ts_chams_arms = new ToggleSwitch ("arms", LOC (ts_chams_players->position.x + ts_chams_players->size.x + 10, ts_chams_players->position.y), 35, &Settings::ESP::Chams::arms);
 	AddComponent (ts_chams_arms);
 	
-	ts_recoil_crosshair = new ToggleSwitch ("recoil crosshair", LOC (10, ts_chams_arms->position.y + ts_chams_arms->size.y + 10), 35, &Settings::Recoilcrosshair::enabled);
+	ba_crosshair = new Banner ("Crosshair", LOC (10, ts_chams_arms->position.y + ts_chams_arms->size.y + 10), size.x - 20);
+	AddComponent (ba_crosshair);
+	
+	ts_recoil_crosshair = new ToggleSwitch ("recoil crosshair", LOC (10, ba_crosshair->position.y + ba_crosshair->size.y + 10), 35, &Settings::Recoilcrosshair::enabled);
 	AddComponent (ts_recoil_crosshair);
 	
-	ts_fov_crosshair = new ToggleSwitch ("fov crosshair", LOC (10, ts_recoil_crosshair->position.y + ts_recoil_crosshair->size.y + 10), 35, &Settings::ESP::FOVCrosshair::enabled);
+	ts_fov_crosshair = new ToggleSwitch ("fov crosshair", LOC (ts_recoil_crosshair->position.x + ts_recoil_crosshair->size.x + 10, ts_recoil_crosshair->position.y), 35, &Settings::ESP::FOVCrosshair::enabled);
 	AddComponent (ts_fov_crosshair);
 	
-	ts_bomb = new ToggleSwitch ("bomb esp", LOC (10, ts_fov_crosshair->position.y + ts_fov_crosshair->size.y + 10), 35, &Settings::ESP::Bomb::enabled);
+	ba_other = new Banner ("Other", LOC (10, ts_fov_crosshair->position.y + ts_fov_crosshair->size.y + 10), size.x - 20);
+	AddComponent (ba_other);
+	
+	ts_bomb = new ToggleSwitch ("bomb esp", LOC (10, ba_other->position.y + ba_other->size.y + 10), 35, &Settings::ESP::Bomb::enabled);
 	AddComponent (ts_bomb);
 	
 	Hide ();
--- src/WINDOWS/esp_window.h
+++ src/WINDOWS/esp_window.h
@@ -12,15 +12,19 @@ class ESPWindow : public Window
 private:
 	ToggleSwitch* ts_esp_enabled;
 	ToggleSwitch* ts_visibility_check;
+	Banner*	ba_player_info;
 	ToggleSwitch* ts_walls;
 	ToggleSwitch* ts_tracer;
 	ToggleSwitch* ts_name;
 	ToggleSwitch* ts_health;
 	ToggleSwitch* ts_bones;
+	Banner*	ba_chams;
 	ToggleSwitch* ts_chams_players;
 	ToggleSwitch* ts_chams_arms;
+	Banner*	ba_crosshair;
 	ToggleSwitch* ts_recoil_crosshair;
 	ToggleSwitch* ts_fov_crosshair;
+	Banner*	ba_other;
 	ToggleSwitch* ts_bomb;
 public:
 	ESPWindow (std::string title, Vector2D size, Vector2D position, Color backgroundColor);
