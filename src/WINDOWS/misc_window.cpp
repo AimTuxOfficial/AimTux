8237a92bf530694456a7915fb8597f6bc39291d4
McSwaggens <McSwaggens@protonmail.com>
2016-10-15 23:55:24 +1100

    Cleaned misc window code

--- src/WINDOWS/misc_window.cpp
+++ src/WINDOWS/misc_window.cpp
@@ -8,28 +8,28 @@ MiscWindow::MiscWindow (std::string title, Vector2D size, Vector2D position, Col
 	ts_bhop = new ToggleSwitch ("bhop", LOC (10, 10), 35, &Settings::BHop::enabled);
 	AddComponent (ts_bhop);
 	
-	ts_autostrafe = new ToggleSwitch ("auto strafe", LOC (10, ts_bhop->position.y + ts_bhop->size.y + 10), 35, &Settings::AutoStrafe::enabled);
+	ts_autostrafe = new ToggleSwitch ("auto strafe", BELOW (ts_bhop), 35, &Settings::AutoStrafe::enabled);
 	AddComponent (ts_autostrafe);
 	
-	ts_spammer = new ToggleSwitch ("chat spammer", LOC (10, ts_autostrafe->position.y + ts_autostrafe->size.y + 10), 35, &Settings::Spammer::NormalSpammer::enabled);
+	ts_spammer = new ToggleSwitch ("chat spammer", BELOW (ts_autostrafe), 35, &Settings::Spammer::NormalSpammer::enabled);
 	AddComponent (ts_spammer);
 	
-	ts_spammer_kill = new ToggleSwitch ("kill spammer", LOC (10, ts_spammer->position.y + ts_spammer->size.y + 10), 35, &Settings::Spammer::KillSpammer::enabled);
+	ts_spammer_kill = new ToggleSwitch ("kill spammer", BELOW (ts_spammer), 35, &Settings::Spammer::KillSpammer::enabled);
 	AddComponent (ts_spammer_kill);
 
-	ts_airstuck = new ToggleSwitch ("airstuck", LOC (10, ts_spammer_kill->position.y + ts_spammer_kill->size.y + 10), 35, &Settings::Airstuck::enabled);
+	ts_airstuck = new ToggleSwitch ("airstuck", BELOW (ts_spammer_kill), 35, &Settings::Airstuck::enabled);
 	AddComponent (ts_airstuck);
 
-	ts_showranks = new ToggleSwitch ("show ranks", LOC (10, ts_airstuck->position.y + ts_airstuck->size.y + 10), 35, &Settings::ShowRanks::enabled);
+	ts_showranks = new ToggleSwitch ("show ranks", BELOW (ts_airstuck), 35, &Settings::ShowRanks::enabled);
 	AddComponent (ts_showranks);
 
-	ts_showspectators = new ToggleSwitch ("show spectators", LOC (10, ts_showranks->position.y + ts_showranks->size.y + 10), 35, &Settings::ShowSpectators::enabled);
+	ts_showspectators = new ToggleSwitch ("show spectators", BELOW (ts_showranks), 35, &Settings::ShowSpectators::enabled);
 	AddComponent (ts_showspectators);
 
-	ts_noflash_enabled = new ToggleSwitch ("no flash", LOC (10, ts_showspectators->position.y + ts_showspectators->size.y + 10), 35, &Settings::Noflash::enabled);
+	ts_noflash_enabled = new ToggleSwitch ("no flash", BELOW (ts_showspectators), 35, &Settings::Noflash::enabled);
 	AddComponent (ts_noflash_enabled);
 	
-	sl_noflash_value = new Slider ("", LOC (ts_noflash_enabled->position.x + ts_noflash_enabled->size.x + 10, ts_noflash_enabled->position.y), LOC (size.x - ts_noflash_enabled->size.x - 30, 35), &Settings::Noflash::value, 0.0f, 255.0f);
+	sl_noflash_value = new Slider ("", STACK (ts_noflash_enabled), LOC (size.x - ts_noflash_enabled->size.x - 30, 35), &Settings::Noflash::value, 0.0f, 255.0f);
 	AddComponent (sl_noflash_value);
 	
 	Hide ();
