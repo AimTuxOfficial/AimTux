8b8c0e23a4c51f4dd93c5d51dfb3c3886483213c
McSwaggens <McSwaggens@protonmail.com>
2016-10-15 23:48:33 +1100

    Updated ts_spammer setting namespace

--- src/WINDOWS/misc_window.cpp
+++ src/WINDOWS/misc_window.cpp
@@ -11,7 +11,7 @@ MiscWindow::MiscWindow (std::string title, Vector2D size, Vector2D position, Col
 	ts_autostrafe = new ToggleSwitch ("auto strafe", LOC (10, ts_bhop->position.y + ts_bhop->size.y + 10), 35, &Settings::AutoStrafe::enabled);
 	AddComponent (ts_autostrafe);
 	
-	ts_spammer = new ToggleSwitch ("spammer", LOC (10, ts_autostrafe->position.y + ts_autostrafe->size.y + 10), 35, &Settings::Spammer::enabled);
+	ts_spammer = new ToggleSwitch ("spammer", LOC (10, ts_autostrafe->position.y + ts_autostrafe->size.y + 10), 35, &Settings::Spammer::NormalSpammer::enabled);
 	AddComponent (ts_spammer);
 
 	ts_airstuck = new ToggleSwitch ("airstuck", LOC (10, ts_spammer->position.y + ts_spammer->size.y + 10), 35, &Settings::Airstuck::enabled);
