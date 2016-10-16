c0f68d9993987913cb43905bedfd2f2403eca5be
LuK1337 <priv.luk@gmail.com>
2016-10-16 16:55:40 +0200

    Change cursor style

--- src/UI/cursor.cpp
+++ src/UI/cursor.cpp
@@ -7,11 +7,17 @@ void Cursor::Draw ()
 	int x, y;
 	input->GetCursorPosition(&x, &y);
 
-	Color color = Color(255, 255, 255, 255);
+	Color white = Color(255, 255, 255, 255);
+	Color black = Color(0, 0, 0, 255);
 
-	DrawLine(Vector2D(x, y), Vector2D(x + 20, y + 10), color);
-	DrawLine(Vector2D(x, y), Vector2D(x + 10, y + 20), color);
+	for (int i = 10; i < 20; i++)
+	{
+		DrawLine(Vector2D(x, y), Vector2D(x + i, y + 10), white);
+		DrawLine(Vector2D(x, y), Vector2D(x + 10, y + i), white);
+	}
 
-	DrawLine(Vector2D(x + 10, y + 20), Vector2D(x + 10, y + 10), color);
-	DrawLine(Vector2D(x + 10, y + 10), Vector2D(x + 20, y + 10), color);
+	DrawLine(Vector2D(x, y), Vector2D(x + 20, y + 10), black);
+	DrawLine(Vector2D(x, y), Vector2D(x + 10, y + 20), black);
+	DrawLine(Vector2D(x + 10, y + 20), Vector2D(x + 10, y + 10), black);
+	DrawLine(Vector2D(x + 10, y + 10), Vector2D(x + 20, y + 10), black);
 }
