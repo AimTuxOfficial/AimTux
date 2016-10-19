12a44597c42da9871c36b0b4e893ed8ef05e5fd6
LuK1337 <priv.luk@gmail.com>
2016-10-19 16:03:49 +0200

    Disable autostrafe when localplayer is dead

--- src/autostrafe.cpp
+++ src/autostrafe.cpp
@@ -15,6 +15,9 @@ void AutoStrafe::CreateMove(CUserCmd* cmd)
 	if (localplayer->GetMoveType() == MOVETYPE_LADDER || localplayer->GetMoveType() == MOVETYPE_NOCLIP)
 		return;
 
+	if (localplayer->GetLifeState() != LIFE_ALIVE || localplayer->GetHealth() == 0)
+		return;
+
 	if (cmd->mousedx > 1 || cmd->mousedx < -1)
 		cmd->sidemove = cmd->mousedx < 0.f ? -450.f : 450.f;
 }
