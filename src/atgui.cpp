3c1ec33a206640e6a75b980f16d62a426cf19c41
McSwaggens <McSwaggens@protonmail.com>
2016-10-21 03:07:05 +1100

    Changed Main Window title

--- src/atgui.cpp
+++ src/atgui.cpp
@@ -6,7 +6,7 @@ void SetupUI()
 	engine->GetScreenSize(x, y);
 	
 	// Main window
-	main_window = new MainWindow("AimTux [NEW UI IN DEVELOPMENT]", LOC(1280 - 20, 720 - 20), LOC((x / 2) - (1280 - 20) / 2, (y / 2) - (720 - 20) / 2), Color(0, 0, 0, 0));
+	main_window = new MainWindow("AimTux Settings", LOC(1280 - 20, 720 - 20), LOC((x / 2) - (1280 - 20) / 2, (y / 2) - (720 - 20) / 2), Color(0, 0, 0, 0));
 	gui->AddWindow(main_window);
 
 	// Menu window
