45633b1d2aba660c0ccd8ed44b25ac1fd8a61797
LuK1337 <priv.luk@gmail.com>
2016-10-20 14:39:11 +0200

    Update misc panel look

--- src/PANELS/misc_panel.cpp
+++ src/PANELS/misc_panel.cpp
@@ -3,36 +3,43 @@
 MiscPanel::MiscPanel (Vector2D position, Vector2D size)
 	: Panel::Panel (position, size)
 {
-	ts_bhop = new ToggleSwitch ("bhop", LOC (10, 10), 33, &Settings::BHop::enabled);
+	ba_movement = new Banner ("Movement", LOC (10, 10), (size.x - 20) / 2 - 5);
+	AddComponent (ba_movement);
+
+	ts_bhop = new ToggleSwitch ("bhop", BELOW (ba_movement), 33, &Settings::BHop::enabled);
 	AddComponent (ts_bhop);
 	
 	ts_autostrafe = new ToggleSwitch ("auto strafe", BELOW (ts_bhop), 33, &Settings::AutoStrafe::enabled);
 	AddComponent (ts_autostrafe);
-	
-	ts_spammer = new ToggleSwitch ("chat spammer", BELOW (ts_autostrafe), 33, &Settings::Spammer::NormalSpammer::enabled);
-	AddComponent (ts_spammer);
-	
-	ts_spammer_kill = new ToggleSwitch ("kill spammer", STACK (ts_spammer), 33, &Settings::Spammer::KillSpammer::enabled);
-	AddComponent (ts_spammer_kill);
 
-	ts_airstuck = new ToggleSwitch ("airstuck", BELOW (ts_spammer), 33, &Settings::Airstuck::enabled);
+	ts_airstuck = new ToggleSwitch ("airstuck", BELOW (ts_autostrafe), 33, &Settings::Airstuck::enabled);
 	AddComponent (ts_airstuck);
 
-	ts_showranks = new ToggleSwitch ("show ranks", BELOW (ts_airstuck), 33, &Settings::ShowRanks::enabled);
-	AddComponent (ts_showranks);
-
-	ts_showspectators = new ToggleSwitch ("show spectators", STACK (ts_showranks), 33, &Settings::ShowSpectators::enabled);
-	AddComponent (ts_showspectators);
-
-	ts_noflash = new ToggleSwitch ("no flash", BELOW (ts_showranks), 33, &Settings::Noflash::enabled);
-	AddComponent (ts_noflash);
+	ba_spammer = new Banner ("Spammer", BELOW (ts_airstuck), (size.x - 20) / 2 - 5);
+	AddComponent (ba_spammer);
 	
-	sl_noflash_value = new Slider ("", STACK (ts_noflash), LOC (size.x - ts_noflash->size.x - 30, 33), &Settings::Noflash::value, 0.0f, 255.0f);
+	ts_spammer = new ToggleSwitch ("chat spammer", BELOW (ba_spammer), 33, &Settings::Spammer::NormalSpammer::enabled);
+	AddComponent (ts_spammer);
+	
+	ts_spammer_kill = new ToggleSwitch ("kill spammer", BELOW (ts_spammer), 33, &Settings::Spammer::KillSpammer::enabled);
+	AddComponent (ts_spammer_kill);
+
+	ba_other = new Banner ("Other", BELOW (ts_spammer_kill), (size.x - 20) / 2 - 5);
+	AddComponent (ba_other);
+
+	ts_noflash = new ToggleSwitch ("no flash", BELOW (ba_other), 33, &Settings::Noflash::enabled);
+	AddComponent (ts_noflash);
+
+	sl_noflash_value = new Slider ("", STACK (ts_noflash), LOC ((size.x / 2) - ts_noflash->size.x - 30, 33), &Settings::Noflash::value, 0.0f, 255.0f);
 	AddComponent (sl_noflash_value);
 
-    // clantag
+	ts_showranks = new ToggleSwitch ("show ranks", BELOW (ts_noflash), 33, &Settings::ShowRanks::enabled);
+	AddComponent (ts_showranks);
 
-	ts_clantag = new ToggleSwitch ("custom clantag", BELOW (ts_noflash), 33, &Settings::ClanTagChanger::enabled);
+	ts_showspectators = new ToggleSwitch ("show spectators", BELOW (ts_showranks), 33, &Settings::ShowSpectators::enabled);
+	AddComponent (ts_showspectators);
+
+	ts_clantag = new ToggleSwitch ("custom clantag", BELOW (ts_showspectators), 33, &Settings::ClanTagChanger::enabled);
 	AddComponent (ts_clantag);
 
 	tb_clantag = new TextBox ("clantag", &Settings::ClanTagChanger::value, STACK (ts_clantag), LOC (270, 33));
--- src/PANELS/misc_panel.h
+++ src/PANELS/misc_panel.h
@@ -9,19 +9,21 @@ class MiscPanel;
 class MiscPanel : public Panel
 {
 private:
+	Banner* ba_movement;
 	ToggleSwitch* ts_bhop;
 	ToggleSwitch* ts_autostrafe;
+	ToggleSwitch* ts_airstuck;
+	Banner* ba_spammer;
 	ToggleSwitch* ts_spammer;
 	ToggleSwitch* ts_spammer_kill;
-	ToggleSwitch* ts_airstuck;
-	ToggleSwitch* ts_showranks;
-	ToggleSwitch* ts_showspectators;
+	Banner* ba_other;
 	ToggleSwitch* ts_noflash;
 	Slider* sl_noflash_value;
+	ToggleSwitch* ts_showranks;
+	ToggleSwitch* ts_showspectators;
 	ToggleSwitch* ts_clantag;
 	TextBox* tb_clantag;
 	ToggleSwitch* ts_clantag_animation;
 public:
 	MiscPanel (Vector2D position, Vector2D size);
-	void SetClanTag ();
 };
