18f4f2bd85ab8f9ef31269bf0b18ee0853ed1385
LuK1337 <priv.luk@gmail.com>
2016-10-20 14:56:09 +0200

    Update visuals panel look

--- src/PANELS/esp_panel.cpp
+++ src/PANELS/esp_panel.cpp
@@ -3,13 +3,13 @@
 ESPPanel::ESPPanel (Vector2D position, Vector2D size)
 	: Panel::Panel (position, size)
 {
-	ts_esp_enabled = new ToggleSwitch ("esp", LOC (10, 10), 33, &Settings::ESP::enabled);
+	ts_esp_enabled = new ToggleSwitch ("enabled", LOC (10, 10), 33, &Settings::ESP::enabled);
 	AddComponent (ts_esp_enabled);
 	
-	ba_player_info = new Banner ("Player Visuals", BELOW (ts_esp_enabled), size.x - 20);
-	AddComponent (ba_player_info);
+	ba_esp = new Banner ("ESP", BELOW (ts_esp_enabled), (size.x - 20) / 2 - 5);
+	AddComponent (ba_esp);
 	
-	ts_walls = new ToggleSwitch ("walls", BELOW (ba_player_info), 33, &Settings::ESP::Walls::enabled);
+	ts_walls = new ToggleSwitch ("walls", BELOW (ba_esp), 33, &Settings::ESP::Walls::enabled);
 	AddComponent (ts_walls);
 	
 	ts_tracer = new ToggleSwitch ("tracer", BELOW (ts_walls), 33, &Settings::ESP::Tracer::enabled);
@@ -26,39 +26,47 @@ ESPPanel::ESPPanel (Vector2D position, Vector2D size)
 	
 	ts_visibility_check = new ToggleSwitch ("visibility check", BELOW (ts_bones), 33, &Settings::ESP::visibility_check);
 	AddComponent (ts_visibility_check);
-	
-	ba_chams = new Banner ("Chams", BELOW (ts_visibility_check), size.x - 20);
-	AddComponent (ba_chams);
-	
-	ts_chams_players = new ToggleSwitch ("players", BELOW (ba_chams), 33, &Settings::ESP::Chams::players);
-	AddComponent (ts_chams_players);
-	
-	ts_chams_arms = new ToggleSwitch ("arms", STACK (ts_chams_players), 33, &Settings::ESP::Chams::arms);
-	AddComponent (ts_chams_arms);
 
-	ts_rainbow_chams_hands = new ToggleSwitch ("rainbow arms", STACK (ts_chams_arms), 33, &Settings::ESP::Chams::rainbow_arms);
-	AddComponent (ts_rainbow_chams_hands);
-
-	ts_visibility_check_chams = new ToggleSwitch ("visibility check", STACK (ts_rainbow_chams_hands), 33, &Settings::ESP::Chams::visibility_check);
-	AddComponent (ts_visibility_check_chams);
-	
-	ba_crosshair = new Banner ("Crosshair", BELOW (ts_chams_players), size.x - 20);
+	ba_crosshair = new Banner ("Crosshair", BELOW (ts_visibility_check), (size.x - 20) / 2 - 5);
 	AddComponent (ba_crosshair);
 	
 	ts_recoil_crosshair = new ToggleSwitch ("recoil crosshair", BELOW (ba_crosshair), 33, &Settings::Recoilcrosshair::enabled);
 	AddComponent (ts_recoil_crosshair);
 	
-	ts_fov_crosshair = new ToggleSwitch ("fov crosshair", STACK (ts_recoil_crosshair), 33, &Settings::ESP::FOVCrosshair::enabled);
+	ts_fov_crosshair = new ToggleSwitch ("fov crosshair", BELOW (ts_recoil_crosshair), 33, &Settings::ESP::FOVCrosshair::enabled);
 	AddComponent (ts_fov_crosshair);
-	
-	ba_other = new Banner ("Other", BELOW (ts_recoil_crosshair), size.x - 20);
+
+	ba_other = new Banner ("Other", BELOW (ts_fov_crosshair), (size.x - 20) / 2 - 5);
 	AddComponent (ba_other);
-	
+
 	ts_bomb = new ToggleSwitch ("bomb esp", BELOW (ba_other), 33, &Settings::ESP::Bomb::enabled);
 	AddComponent (ts_bomb);
 
-	ts_weapons = new ToggleSwitch ("weapons esp", STACK (ts_bomb), 33, &Settings::ESP::Weapons::enabled);
+	ts_weapons = new ToggleSwitch ("weapons esp", BELOW (ts_bomb), 33, &Settings::ESP::Weapons::enabled);
 	AddComponent (ts_weapons);
+
+	ba_chams = new Banner ("Chams", LOC((size.x / 2) + 5, ba_esp->position.y), ((size.x - 20) / 2) - 5);
+	AddComponent (ba_chams);
+
+	ts_chams_players = new ToggleSwitch ("players", BELOW (ba_chams), 33, &Settings::ESP::Chams::players);
+	AddComponent (ts_chams_players);
+
+	ts_chams_arms = new ToggleSwitch ("arms", BELOW (ts_chams_players), 33, &Settings::ESP::Chams::arms);
+	AddComponent (ts_chams_arms);
+
+	ts_rainbow_chams_hands = new ToggleSwitch ("rainbow arms", BELOW (ts_chams_arms), 33, &Settings::ESP::Chams::rainbow_arms);
+	AddComponent (ts_rainbow_chams_hands);
+
+	ts_visibility_check_chams = new ToggleSwitch ("visibility check", BELOW (ts_rainbow_chams_hands), 33, &Settings::ESP::Chams::visibility_check);
+	AddComponent (ts_visibility_check_chams);
+
+	lb_chamstype = new StackedListBox<ChamsType>("aimbone", BELOW (ts_visibility_check_chams), ((size.x - 20) / 2) - 5, 3, &Settings::ESP::Chams::type, std::vector<LB_Element>
+			{
+					LB_Element ("NORMAL", CHAMS),
+					LB_Element ("FLAT", CHAMS_FLAT),
+			}
+	);
+	AddComponent (lb_chamstype);
 	
 	Hide ();
 }
--- src/PANELS/esp_panel.h
+++ src/PANELS/esp_panel.h
@@ -10,24 +10,25 @@ class ESPPanel : public Panel
 {
 private:
 	ToggleSwitch* ts_esp_enabled;
-	ToggleSwitch* ts_visibility_check;
-	Banner*	ba_player_info;
+	Banner*	ba_esp;
 	ToggleSwitch* ts_walls;
 	ToggleSwitch* ts_tracer;
 	ToggleSwitch* ts_name;
 	ToggleSwitch* ts_health;
 	ToggleSwitch* ts_bones;
-	Banner*	ba_chams;
-	ToggleSwitch* ts_chams_players;
-	ToggleSwitch* ts_chams_arms;
-	ToggleSwitch* ts_rainbow_chams_hands;
-	ToggleSwitch* ts_visibility_check_chams;
+	ToggleSwitch* ts_visibility_check;
 	Banner*	ba_crosshair;
 	ToggleSwitch* ts_recoil_crosshair;
 	ToggleSwitch* ts_fov_crosshair;
 	Banner*	ba_other;
 	ToggleSwitch* ts_bomb;
 	ToggleSwitch* ts_weapons;
+	Banner*	ba_chams;
+	ToggleSwitch* ts_chams_players;
+	ToggleSwitch* ts_chams_arms;
+	ToggleSwitch* ts_rainbow_chams_hands;
+	ToggleSwitch* ts_visibility_check_chams;
+	StackedListBox<ChamsType>* lb_chamstype;
 public:
 	ESPPanel (Vector2D position, Vector2D size);
 };
