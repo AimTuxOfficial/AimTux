386ecd893f0e3658d31160061e103bb700b3e674
McSwaggens <McSwaggens@protonmail.com>
2016-10-23 05:59:49 +1100

    Added basic PopupColorWindow

--- src/WINDOWS/popup_window.cpp
+++ src/WINDOWS/popup_window.cpp
@@ -14,3 +14,29 @@ void PopupWindow::bn_cancel_clicked ()
 {
 	this->Destroy ();
 }
+
+
+
+
+PopupColorWindow::PopupColorWindow (std::string title, Vector2D size, Vector2D position, Color backgroundColor, Color* color)
+	: PopupWindow::PopupWindow (title, size, position, backgroundColor)
+{
+	this->color = color;
+	this->old_color = *this->color;
+	
+	bn_save = new OutlinedButton ("Save", STACK (bn_cancel), LOC ((size.x / 2) - (20), 35));
+	bn_cancel->OnClickedEvent = MFUNC (&PopupColorWindow::bn_cancel_revert_clicked, this);
+	bn_save->OnClickedEvent = MFUNC (&PopupColorWindow::bn_save_clicked, this);
+	AddComponent (bn_save);
+}
+
+void PopupColorWindow::bn_cancel_revert_clicked ()
+{
+	*this->color = this->old_color;
+	this->Destroy ();
+}
+
+void PopupColorWindow::bn_save_clicked ()
+{
+	this->Destroy ();
+}
--- src/WINDOWS/popup_window.h
+++ src/WINDOWS/popup_window.h
@@ -8,9 +8,21 @@ class PopupWindow;
 
 class PopupWindow : public Window
 {
-private:
+protected:
 	Button* bn_cancel;
 public:
 	PopupWindow (std::string title, Vector2D size, Vector2D position, Color backgroundColor);
 	void bn_cancel_clicked ();
+};
+
+class PopupColorWindow : public PopupWindow
+{
+protected:
+	Color old_color;
+	Color* color;
+	Button* bn_save;
+public:
+	PopupColorWindow (std::string title, Vector2D size, Vector2D position, Color backgroundColor, Color* color);
+	void bn_cancel_revert_clicked ();
+	void bn_save_clicked ();
 };
