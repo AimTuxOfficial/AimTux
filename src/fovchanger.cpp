47d489eb4c62e8ff6b8578a244a036ccec2c9df8
LuK1337 <priv.luk@gmail.com>
2016-10-26 17:55:49 +0200

    Use guard checks in FOV changer

--- src/fovchanger.cpp
+++ src/fovchanger.cpp
@@ -8,18 +8,18 @@ float Settings::FOVChanger::viewmodel_value = 90.f;
 
 void FOVChanger::RenderView(CViewSetup & setup, CViewSetup & hudViewSetup, unsigned int nClearFlags, int whatToDraw)
 {
-	if (Settings::FOVChanger::enabled && engine->IsInGame())
-	{
-		C_BasePlayer* localplayer = (C_BasePlayer*)entitylist->GetClientEntity(engine->GetLocalPlayer());
+	if (!Settings::FOVChanger::enabled)
+			return;
 
-		if (localplayer)
-		{
-			if (!localplayer->IsScoped())
-			{
-				setup.fov = Settings::FOVChanger::value;
-			}
+	if (!engine->IsInGame())
+		return;
 
-			setup.fovViewmodel = Settings::FOVChanger::viewmodel_value;
-		}
-	}
+	C_BasePlayer* localplayer = (C_BasePlayer*)entitylist->GetClientEntity(engine->GetLocalPlayer());
+	if (!localplayer)
+		return;
+
+	if (!localplayer->IsScoped())
+		setup.fov = Settings::FOVChanger::value;
+
+	setup.fovViewmodel = Settings::FOVChanger::viewmodel_value;
 }
