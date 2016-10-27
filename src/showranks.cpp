d799f032d74c13141c052d81eb61a18c867eb8af
LuK1337 <priv.luk@gmail.com>
2016-10-27 18:56:56 +0200

    Add ingame check to showranks

--- src/showranks.cpp
+++ src/showranks.cpp
@@ -7,6 +7,9 @@ void ShowRanks::CreateMove(CUserCmd* cmd)
 	if (!Settings::ShowRanks::enabled)
 		return;
 
+	if (!engine->IsInGame())
+		return;
+
 	if (cmd->buttons & IN_SCORE)
 	{
 		float input[3] = { 0.f };
