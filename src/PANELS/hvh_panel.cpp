ab2d895633d04e761cd4c95096b639c8fcf44848
McSwaggens <McSwaggens@protonmail.com>
2016-11-03 00:33:14 +1100

    Organised hvh menu

--- src/PANELS/hvh_panel.cpp
+++ src/PANELS/hvh_panel.cpp
@@ -7,7 +7,7 @@ HVHPanel::HVHPanel (Vector2D position, Vector2D size)
 	AddComponent (ts_antiaim_x);
 	
 	
-	lb_antiaim_x = new ListBox<AntiAimType_X> ("anti aim type", LOC (10, ts_antiaim_x->position.y + ts_antiaim_x->size.y + 10), 170, &Settings::AntiAim::type_X, std::vector<LB_Element>
+	lb_antiaim_x = new ListBox<AntiAimType_X> ("anti aim type", BELOW (ts_antiaim_x), 170, &Settings::AntiAim::type_X, std::vector<LB_Element>
 		{
 			LB_Element ("UP", STATIC_UP),
 			LB_Element ("DOWN", STATIC_DOWN)
@@ -16,8 +16,10 @@ HVHPanel::HVHPanel (Vector2D position, Vector2D size)
 	AddComponent (lb_antiaim_x);
 	
 	
+	ts_antiaim_y = new ToggleSwitch ("y axis", BELOW (lb_antiaim_x), 33, &Settings::AntiAim::enabled_Y);
+	AddComponent (ts_antiaim_y);
 	
-	lb_antiaim_y = new ListBox<AntiAimType_Y> ("anti aim type", LOC (lb_antiaim_x->position.x + lb_antiaim_x->size.x + 10, lb_antiaim_x->position.y), 170, &Settings::AntiAim::type_Y, std::vector<LB_Element>
+	lb_antiaim_y = new ListBox<AntiAimType_Y> ("anti aim type", BELOW (ts_antiaim_y), 170, &Settings::AntiAim::type_Y, std::vector<LB_Element>
 		{
 			LB_Element ("SLOW SPIN", SPIN_SLOW),
 			LB_Element ("FAST SPIN", SPIN_FAST),
@@ -32,10 +34,8 @@ HVHPanel::HVHPanel (Vector2D position, Vector2D size)
 	);
 	AddComponent (lb_antiaim_y);
 	
-	ts_antiaim_y = new ToggleSwitch ("y axis", LOC (lb_antiaim_x->position.x + lb_antiaim_x->size.x + 10, lb_antiaim_y->position.y - 45), 33, &Settings::AntiAim::enabled_Y);
-	AddComponent (ts_antiaim_y);
 
-	lb_bone_select_multi = new MS_ListBox<Hitbox> ("Autowall bones", LOC (lb_antiaim_y->position.x + lb_antiaim_y->size.x + 10, lb_antiaim_y->position.y), 170, &Settings::Aimbot::AutoWall::bones, std::vector<LB_Element>
+	lb_bone_select_multi = new MS_ListBox<Hitbox> ("Autowall bones", STACK (lb_antiaim_x) - BELOW (ts_antiaim_x) + LOC (10, 10), 170, &Settings::Aimbot::AutoWall::bones, std::vector<LB_Element>
 		{
 			LB_Element ("HEAD", HITBOX_HEAD),
 			LB_Element ("NECK", HITBOX_NECK),
