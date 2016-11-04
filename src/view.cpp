6cc4a6b1af6d6324ef1d64fe43dc1a06832aa2aa
LuK1337 <priv.luk@gmail.com>
2016-11-04 14:44:37 +0100

    Fix view / no punch code style

--- src/view.cpp
+++ src/view.cpp
@@ -4,18 +4,23 @@ bool Settings::View::NoPunch::enabled = false;
 
 QAngle old_punch_angle;
 
-
 void View::NoPunch::FrameStageNotify (ClientFrameStage_t stage)
 {
 	if (!Settings::View::NoPunch::enabled)
 		return;
-	
+
+	if (!engine->IsInGame())
+		return;
+
+	if (stage != FRAME_RENDER_START)
+		return;
+
 	C_BasePlayer* localplayer = (C_BasePlayer*)entitylist->GetClientEntity(engine->GetLocalPlayer());
-	
+
 	QAngle* punch_angle = localplayer->GetViewPunchAngle ();
 	old_punch_angle = QAngle (0, 0, 0);
-	
-	if (stage == FRAME_RENDER_START && punch_angle && engine->IsInGame())
+
+	if (punch_angle)
 	{
 		old_punch_angle = *punch_angle;
 		*punch_angle = QAngle (0, 0, 0);
@@ -26,25 +31,27 @@ void View::NoPunch::PostFrameStageNotify(ClientFrameStage_t stage)
 {
 	if (!Settings::View::NoPunch::enabled)
 		return;
-	
-	
+
+	if (!engine->IsInGame())
+		return;
+
+	if (stage != FRAME_RENDER_START)
+		return;
+
 	C_BasePlayer* localplayer = (C_BasePlayer*)entitylist->GetClientEntity(engine->GetLocalPlayer());
+	QAngle* punch_angle = localplayer->GetViewPunchAngle();
 	
-	QAngle* punch_angle = localplayer->GetViewPunchAngle ();
-	
-	if (stage == FRAME_RENDER_START && punch_angle && engine->IsInGame())
-	{
+	if (punch_angle)
 		*punch_angle = old_punch_angle;
-	}
 }
 
 
 void View::FrameStageNotify(ClientFrameStage_t stage)
 {
-	View::NoPunch::FrameStageNotify (stage);
+	View::NoPunch::FrameStageNotify(stage);
 }
 
 void View::PostFrameStageNotify(ClientFrameStage_t stage)
 {
-	View::NoPunch::PostFrameStageNotify (stage);
+	View::NoPunch::PostFrameStageNotify(stage);
 }
