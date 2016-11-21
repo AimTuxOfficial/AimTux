619e9664738d4cb7734740cf57faeba7d7ae920d
LuK1337 <priv.luk@gmail.com>
2016-11-21 15:13:46 +0100

    Add /add config/ to UI

--- src/PANELS/config_panel.cpp
+++ src/PANELS/config_panel.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "config_panel.h"
 
 ConfigPanel::ConfigPanel (Vector2D position, Vector2D size)
@@ -9,12 +10,17 @@ ConfigPanel::ConfigPanel (Vector2D position, Vector2D size)
 	bn_remove->OnClickedEvent = MFUNC (&ConfigPanel::bn_remove_clicked, this);
 	bn_save = new OutlinedButton ("Save", STACK (bn_remove), LOC (150, 30));
 	bn_save->OnClickedEvent = MFUNC (&ConfigPanel::bn_save_clicked, this);
-	lb_configs = new ListBox_Config ("Configs", BELOW (bn_refresh), bn_save->position.x + bn_save->size.x - 10);
+	vtb_add = new ValueTextBox ("Config name", "", BELOW (bn_refresh), LOC(310, 30));
+	ob_add = new OutlinedButton ("Add", STACK (vtb_add), LOC(150, 30));
+	ob_add->OnClickedEvent = MFUNC (&ConfigPanel::ob_add_clicked, this);
+	lb_configs = new ListBox_Config ("Configs", BELOW (vtb_add), bn_save->position.x + bn_save->size.x - 10);
 
 	AddComponent (lb_configs);
 	AddComponent (bn_refresh);
 	AddComponent (bn_remove);
 	AddComponent (bn_save);
+	AddComponent (ob_add);
+	AddComponent (vtb_add);
 
 	Hide ();
 }
@@ -43,4 +49,23 @@ void ConfigPanel::bn_save_clicked ()
 		return;
 
 	Settings::LoadDefaultsOrSave(*current_config);
+}
+
+void ConfigPanel::ob_add_clicked()
+{
+	if (vtb_add->text.length() == 0)
+		return;
+
+	pstring path = getenv("HOME");
+	path << "/.config" << "/AimTux/" << vtb_add->text;
+
+	if (DoesFileExist(path.c_str()))
+		return;
+
+	mkdir(path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
+	Config new_config(vtb_add->text.c_str(), path.c_str());
+	Settings::LoadDefaultsOrSave(new_config);
+
+	bn_refresh_clicked ();
 }
