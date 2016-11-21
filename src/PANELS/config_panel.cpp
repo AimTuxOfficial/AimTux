1ab25b43bb3eaf0c4de8298830822dbcdb166592
LuK1337 <priv.luk@gmail.com>
2016-11-21 14:55:04 +0100

    Add save button

--- src/PANELS/config_panel.cpp
+++ src/PANELS/config_panel.cpp
@@ -3,16 +3,19 @@
 ConfigPanel::ConfigPanel (Vector2D position, Vector2D size)
 	: Panel::Panel (position, size)
 {
-	lb_configs = new ListBox_Config ("Configs", LOC (size.x / 3, 10), size.x / 3);
-	bn_refresh = new OutlinedButton ("Refresh", STACK (lb_configs), LOC (150, 30));
+	bn_refresh = new OutlinedButton ("Refresh", LOC (10, 10), LOC (150, 30));
 	bn_refresh->OnClickedEvent = MFUNC (&ConfigPanel::bn_refresh_clicked, this);
-	bn_remove = new OutlinedButton ("Remove", BELOW (bn_refresh), LOC (150, 30));
+	bn_remove = new OutlinedButton ("Remove", STACK (bn_refresh), LOC (150, 30));
 	bn_remove->OnClickedEvent = MFUNC (&ConfigPanel::bn_remove_clicked, this);
-	
+	bn_save = new OutlinedButton ("Save", STACK (bn_remove), LOC (150, 30));
+	bn_save->OnClickedEvent = MFUNC (&ConfigPanel::bn_save_clicked, this);
+	lb_configs = new ListBox_Config ("Configs", BELOW (bn_refresh), bn_save->position.x + bn_save->size.x - 10);
+
 	AddComponent (lb_configs);
 	AddComponent (bn_refresh);
 	AddComponent (bn_remove);
-	
+	AddComponent (bn_save);
+
 	Hide ();
 }
 
@@ -32,4 +35,12 @@ void ConfigPanel::bn_remove_clicked ()
 	lb_configs->Reload();
 	
 	current_config = nullptr;
+}
+
+void ConfigPanel::bn_save_clicked ()
+{
+	if (current_config == nullptr)
+		return;
+
+	Settings::LoadDefaultsOrSave(*current_config);
 }
--- src/PANELS/config_panel.h
+++ src/PANELS/config_panel.h
@@ -13,8 +13,10 @@ private:
 	ListBox_Config* lb_configs;
 	OutlinedButton* bn_refresh;
 	OutlinedButton* bn_remove;
+	OutlinedButton* bn_save;
 public:
 	ConfigPanel(Vector2D position, Vector2D size);
 	void bn_refresh_clicked ();
 	void bn_remove_clicked ();
+	void bn_save_clicked ();
 };
