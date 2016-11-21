e8a914d9dc5448dffdd79ec800e36c955a9fb821
Daniel Jones <McSwaggens@protonmail.com>
2016-11-21 22:06:52 +1100

    Added Reload button to config menu

--- src/PANELS/config_panel.cpp
+++ src/PANELS/config_panel.cpp
@@ -4,9 +4,17 @@ ConfigPanel::ConfigPanel (Vector2D position, Vector2D size)
 	: Panel::Panel (position, size)
 {
 	lb_configs = new ListBox_Config ("Configs", LOC (size.x / 3, 10), size.x / 3);
-	
+	bn_refresh = new OutlinedButton ("Refresh", STACK (lb_configs), LOC (150, 30));
+	bn_refresh->OnClickedEvent = MFUNC (&ConfigPanel::bn_refresh_clicked, this);
 	
 	AddComponent (lb_configs);
+	AddComponent (bn_refresh);
 	
 	Hide ();
+}
+
+void ConfigPanel::bn_refresh_clicked ()
+{
+	Settings::LoadSettings ();
+	lb_configs->Reload();
 }
--- src/PANELS/config_panel.h
+++ src/PANELS/config_panel.h
@@ -11,6 +11,8 @@ class ConfigPanel : public Panel
 {
 private:
 	ListBox_Config* lb_configs;
+	OutlinedButton* bn_refresh;
 public:
 	ConfigPanel(Vector2D position, Vector2D size);
+	void bn_refresh_clicked ();
 };
