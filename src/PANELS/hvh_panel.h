5525b9efdead1230c698d5aef4949cea1a506c82
Barclay <barclaycsgo@gmail.com>
2016-12-02 22:55:49 +0000

    Fixed what others broke. Though this might be broken too. Will need feedback.

--- src/PANELS/hvh_panel.h
+++ src/PANELS/hvh_panel.h
@@ -14,6 +14,7 @@ private:
 	ComboBox<AntiAimType_X>* cb_antiaim_x;
 	ToggleSwitch* ts_antiaim_x;
 	ComboBox<AntiAimType_Y>* cb_antiaim_y;
+	ComboBox<AntiAimType_Y>* cb_fake_antiaim_y;
 	ToggleSwitch* ts_antiaim_y;
 	ToggleSwitch* ts_edge_hide;
 	Slider* sl_edge_distance;
@@ -29,6 +30,9 @@ private:
 	ToggleSwitch* ts_autowall;
 	Slider* sl_autowall;
 	MS_ListBox<Hitbox>* lb_bone_select_multi;
+
+	OutlinedButton* bn_switchAA;
 public:
 	HVHPanel (Vector2D position, Vector2D size);
+	void bn_switchAA_clicked();
 };
