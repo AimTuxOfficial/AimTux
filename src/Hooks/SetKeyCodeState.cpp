54dffb962bc9531abb80c7e700a63f8a90901c64
LuK1337 <priv.luk@gmail.com>
2016-12-10 19:50:46 +0100

    Don't listen for UI hotkey in SetKeyCodeState hook

--- src/Hooks/SetKeyCodeState.cpp
+++ src/Hooks/SetKeyCodeState.cpp
@@ -2,8 +2,5 @@
 
 void Hooks::SetKeyCodeState(void* thisptr, ButtonCode_t code, bool bPressed)
 {
-	if (code == ButtonCode_t::KEY_INSERT && bPressed)
-		UI::isVisible = !UI::isVisible;
-
 	inputInternal_vmt->GetOriginalMethod<SetKeyCodeStateFn>(92)(thisptr, code, bPressed);
 }
