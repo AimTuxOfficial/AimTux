37c02d8bf4ddd456d4b32ce77c6a7c2bafba96e2
SirBarclay <barclaycsgo@gmail.com>
2016-12-14 16:17:07 +0000

    Tweaked size of colors window

--- src/atgui.cpp
+++ src/atgui.cpp
@@ -211,7 +211,7 @@ void ColorsWindow()
 			};
 	static int colorSelected = 0;
 
-	ImGui::SetNextWindowSize(ImVec2(540, 260), ImGuiSetCond_Always);
+	ImGui::SetNextWindowSize(ImVec2(540, 265), ImGuiSetCond_Always);
 	if (ImGui::Begin("Colors", &showColorsWindow, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_ShowBorders | ImGuiWindowFlags_NoResize))
 	{
 		ImGui::Columns(2, NULL, true);
