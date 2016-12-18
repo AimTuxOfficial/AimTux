cd007fa3363dab26bf8c725f0fbc2f8111896d2e
LuK1337 <priv.luk@gmail.com>
2016-12-18 15:42:36 +0100

    Add userid verification for player_connect_full event

--- src/aimbot.cpp
+++ src/aimbot.cpp
@@ -545,5 +545,10 @@ void Aimbot::FireEventClientSide(IGameEvent* event)
 		return;
 
 	if (strcmp(event->GetName(), "player_connect_full") == 0 || strcmp(event->GetName(), "cs_game_disconnected") == 0)
+	{
+		if (event->GetInt("userid") || engine->GetPlayerForUserID(event->GetInt("userid")) != engine->GetLocalPlayer())
+			return;
+
 		Aimbot::Friendlies.clear();
+	}
 }
--- src/resolver.cpp
+++ src/resolver.cpp
@@ -63,5 +63,10 @@ void Resolver::FireEventClientSide(IGameEvent* event)
 		return;
 
 	if (strcmp(event->GetName(), "player_connect_full") == 0 || strcmp(event->GetName(), "cs_game_disconnected") == 0)
+	{
+		if (event->GetInt("userid") || engine->GetPlayerForUserID(event->GetInt("userid")) != engine->GetLocalPlayer())
+			return;
+
 		Resolver::Players.clear();
+	}
 }
