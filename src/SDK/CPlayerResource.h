3ce4ba178747f1fd1e130d65380ec8622310a8ca
LuK1337 <priv.luk@gmail.com>
2016-12-18 20:01:25 +0100

    Use C style casts in C_CSPlayerResource

--- src/SDK/CPlayerResource.h
+++ src/SDK/CPlayerResource.h
@@ -7,66 +7,66 @@ class C_CSPlayerResource : public CPlayerResource
 public:
 	int GetPlayerC4()
 	{
-		return *reinterpret_cast<int*>(reinterpret_cast<uintptr_t>(this) + offsets.DT_CSPlayerResource.m_iPlayerC4);
+		return *(int*)((uintptr_t)this + offsets.DT_CSPlayerResource.m_iPlayerC4);
 	}
 
 	int GetMVPs(int index)
 	{
-		return *reinterpret_cast<int*>(reinterpret_cast<uintptr_t>(this) + offsets.DT_CSPlayerResource.m_iMVPs + index * 4);
+		return *(int*)((uintptr_t)this + offsets.DT_CSPlayerResource.m_iMVPs + index * 4);
 	}
 
 	int GetArmor(int index)
 	{
-		return *reinterpret_cast<int*>(reinterpret_cast<uintptr_t>(this) + offsets.DT_CSPlayerResource.m_iArmor + index * 4);
+		return *(int*)((uintptr_t)this + offsets.DT_CSPlayerResource.m_iArmor + index * 4);
 	}
 
 	int GetScore(int index)
 	{
-		return *reinterpret_cast<int*>(reinterpret_cast<uintptr_t>(this) + offsets.DT_CSPlayerResource.m_iScore + index * 4);
+		return *(int*)((uintptr_t)this + offsets.DT_CSPlayerResource.m_iScore + index * 4);
 	}
 
 	int* GetCompetitiveRanking(int index)
 	{
-		return reinterpret_cast<int*>(reinterpret_cast<uintptr_t>(this) + offsets.DT_CSPlayerResource.m_iCompetitiveRanking + index * 4);
+		return (int*)((uintptr_t)this + offsets.DT_CSPlayerResource.m_iCompetitiveRanking + index * 4);
 	}
 
 	int* GetCompetitiveWins(int index)
 	{
-		return reinterpret_cast<int*>(reinterpret_cast<uintptr_t>(this) + offsets.DT_CSPlayerResource.m_iCompetitiveWins + index * 4);
+		return (int*)((uintptr_t)this + offsets.DT_CSPlayerResource.m_iCompetitiveWins + index * 4);
 	}
 
 	int GetCompTeammateColor(int index)
 	{
-		return *reinterpret_cast<int*>(reinterpret_cast<uintptr_t>(this) + offsets.DT_CSPlayerResource.m_iCompTeammateColor + index * 4);
+		return *(int*)((uintptr_t)this + offsets.DT_CSPlayerResource.m_iCompTeammateColor + index * 4);
 	}
 
 	char* GetClan(int index)
 	{
-		return reinterpret_cast<char*>(reinterpret_cast<uintptr_t>(this) + offsets.DT_CSPlayerResource.m_szClan + index * 16);
+		return (char*)((uintptr_t)this + offsets.DT_CSPlayerResource.m_szClan + index * 16);
 	}
 
 	int* GetActiveCoinRank(int index)
 	{
-		return reinterpret_cast<int*>(reinterpret_cast<uintptr_t>(this) + offsets.DT_CSPlayerResource.m_nActiveCoinRank + index * 4);
+		return (int*)((uintptr_t)this + offsets.DT_CSPlayerResource.m_nActiveCoinRank + index * 4);
 	}
 
 	int* GetMusicID(int index)
 	{
-		return reinterpret_cast<int*>(reinterpret_cast<uintptr_t>(this) + offsets.DT_CSPlayerResource.m_nMusicID + index * 4);
+		return (int*)((uintptr_t)this + offsets.DT_CSPlayerResource.m_nMusicID + index * 4);
 	}
 
 	int* GetPersonaDataPublicCommendsLeader(int index)
 	{
-		return reinterpret_cast<int*>(reinterpret_cast<uintptr_t>(this) + offsets.DT_CSPlayerResource.m_nPersonaDataPublicCommendsLeader + index * 4);
+		return (int*)((uintptr_t)this + offsets.DT_CSPlayerResource.m_nPersonaDataPublicCommendsLeader + index * 4);
 	}
 
 	int* GetPersonaDataPublicCommendsTeacher(int index)
 	{
-		return reinterpret_cast<int*>(reinterpret_cast<uintptr_t>(this) + offsets.DT_CSPlayerResource.m_nPersonaDataPublicCommendsTeacher + index * 4);
+		return (int*)((uintptr_t)this + offsets.DT_CSPlayerResource.m_nPersonaDataPublicCommendsTeacher + index * 4);
 	}
 
 	int* GetPersonaDataPublicCommendsFriendly(int index)
 	{
-		return reinterpret_cast<int*>(reinterpret_cast<uintptr_t>(this) + offsets.DT_CSPlayerResource.m_nPersonaDataPublicCommendsFriendly + index * 4);
+		return (int*)((uintptr_t)this + offsets.DT_CSPlayerResource.m_nPersonaDataPublicCommendsFriendly + index * 4);
 	}
 };
