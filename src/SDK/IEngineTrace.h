38c0b290661b9d13046fd8639b23d67e99cc7ffe
LuK1337 <priv.luk@gmail.com>
2017-01-01 14:37:07 +0100

    Replace ITraceEngine with whole vtable class

--- src/SDK/IEngineTrace.h
+++ src/SDK/IEngineTrace.h
@@ -1,11 +1,24 @@
 #pragma once
 
+class CTraceListData;
+class CPhysCollide;
+class Vector4D;
+class ITraceListData;
+struct virtualmeshlist_t;
+struct BrushSideInfo_t;
+
 enum TraceType_t
 {
 	TRACE_EVERYTHING = 0,
 	TRACE_WORLD_ONLY,
 	TRACE_ENTITIES_ONLY,
 	TRACE_EVERYTHING_FILTER_PROPS,
+};
+
+enum DebugTraceCounterBehavior_t
+{
+	kTRACE_COUNTER_SET = 0,
+	kTRACE_COUNTER_INC,
 };
 
 struct cplane_t
@@ -67,17 +80,11 @@ struct Ray_t
 	void Init(Vector vecStart, Vector vecEnd)
 	{
 		m_Delta = vecEnd - vecStart;
-
 		m_IsSwept = (m_Delta.LengthSqr() != 0);
-
 		m_Extents.x = m_Extents.y = m_Extents.z = 0.0f;
-
 		m_pWorldAxisTransform = NULL;
-
 		m_IsRay = true;
-
 		m_StartOffset.x = m_StartOffset.y = m_StartOffset.z = 0.0f;
-
 		m_Start = vecStart;
 	}
 };
@@ -85,7 +92,7 @@ struct Ray_t
 class ITraceFilter
 {
 public:
-	virtual bool ShouldHitEntity(C_BaseEntity *pEntity, int contentsMask) = 0;
+	virtual bool ShouldHitEntity(C_BaseEntity* pEntity, int contentsMask) = 0;
 	virtual TraceType_t GetTraceType() const = 0;
 };
 
@@ -105,18 +112,85 @@ public:
 	void* pSkip;
 };
 
+class IEntityEnumerator
+{
+public:
+	// This gets called with each handle
+	virtual bool EnumEntity(IHandleEntity* pHandleEntity) = 0;
+};
+
 class IEngineTrace
 {
 public:
-	void TraceRay(const Ray_t &ray, unsigned int fMask, CTraceFilter *pTraceFilter, trace_t *pTrace)
-	{
-		typedef void (* oTraceRay)(void*, const Ray_t&, unsigned int, CTraceFilter *, trace_t*);
-		return getvfunc<oTraceRay>(this, 5)(this, ray, fMask, pTraceFilter, pTrace);
-	}
+	// Returns the contents mask + entity at a particular world-space position
+	virtual int	GetPointContents(const Vector &vecAbsPosition, int contentsMask = MASK_ALL, IHandleEntity** ppEntity = NULL) = 0;
 
-	int GetPointContents(const Vector &vecAbsPosition, int contentsMask = MASK_ALL, IHandleEntity** ppEntity = NULL)
-	{
-		typedef int (* oGetPointContents)(void*, const Vector &vecAbsPosition, int contentsMask, IHandleEntity** ppEntity);
-		return getvfunc<oGetPointContents>(this, 1)(this, vecAbsPosition, contentsMask, ppEntity);
-	}
+	// Returns the contents mask of the world only @ the world-space position (static props are ignored)
+	virtual int	GetPointContents_WorldOnly(const Vector &vecAbsPosition, int contentsMask = MASK_ALL) = 0;
+
+	// Get the point contents, but only test the specific entity. This works
+	// on static props and brush models.
+	//
+	// If the entity isn't a static prop or a brush model, it returns CONTENTS_EMPTY and sets
+	// bFailed to true if bFailed is non-null.
+	virtual int	GetPointContents_Collideable(ICollideable *pCollide, const Vector &vecAbsPosition) = 0;
+
+	// Traces a ray against a particular entity
+	virtual void ClipRayToEntity(const Ray_t &ray, unsigned int fMask, IHandleEntity *pEnt, trace_t *pTrace) = 0;
+
+	// Traces a ray against a particular entity
+	virtual void ClipRayToCollideable(const Ray_t &ray, unsigned int fMask, ICollideable *pCollide, trace_t *pTrace) = 0;
+
+	// A version that simply accepts a ray (can work as a traceline or tracehull)
+	virtual void TraceRay(const Ray_t &ray, unsigned int fMask, ITraceFilter *pTraceFilter, trace_t *pTrace) = 0;
+
+	// A version that sets up the leaf and entity lists and allows you to pass those in for collision.
+	virtual void SetupLeafAndEntityListRay(const Ray_t &ray, ITraceListData *pTraceData) = 0;
+	virtual void SetupLeafAndEntityListBox(const Vector &vecBoxMin, const Vector &vecBoxMax, ITraceListData *pTraceData) = 0;
+	virtual void TraceRayAgainstLeafAndEntityList(const Ray_t &ray, ITraceListData *pTraceData, unsigned int fMask, ITraceFilter *pTraceFilter, trace_t *pTrace) = 0;
+
+	// A version that sweeps a collideable through the world
+	// abs start + abs end represents the collision origins you want to sweep the collideable through
+	// vecAngles represents the collision angles of the collideable during the sweep
+	virtual void SweepCollideable(ICollideable *pCollide, const Vector &vecAbsStart, const Vector &vecAbsEnd, const QAngle &vecAngles, unsigned int fMask, ITraceFilter *pTraceFilter, trace_t *pTrace) = 0;
+
+	// Enumerates over all entities along a ray
+	// If triggers == true, it enumerates all triggers along a ray
+	virtual void EnumerateEntities(const Ray_t &ray, bool triggers, IEntityEnumerator *pEnumerator) = 0;
+
+	// Same thing, but enumerate entitys within a box
+	virtual void EnumerateEntities(const Vector &vecAbsMins, const Vector &vecAbsMaxs, IEntityEnumerator *pEnumerator) = 0;
+
+	// Convert a handle entity to a collideable.  Useful inside enumer
+	virtual ICollideable *GetCollideable(IHandleEntity *pEntity) = 0;
+
+	// HACKHACK: Temp for performance measurments
+	virtual int GetStatByIndex(int index, bool bClear) = 0;
+
+
+	//finds brushes in an AABB, prone to some false positives
+	virtual void GetBrushesInAABB(const Vector &vMins, const Vector &vMaxs, CUtlVector<int> *pOutput, int iContentsMask = 0xFFFFFFFF) = 0;
+
+	//Creates a CPhysCollide out of all displacements wholly or partially contained in the specified AABB
+	virtual CPhysCollide* GetCollidableFromDisplacementsInAABB(const Vector& vMins, const Vector& vMaxs) = 0;
+
+	// gets the number of displacements in the world
+	virtual int GetNumDisplacements() = 0;
+
+	// gets a specific diplacement mesh
+	virtual void GetDisplacementMesh(int nIndex, virtualmeshlist_t *pMeshTriList) = 0;
+
+	//retrieve brush planes and contents, returns true if data is being returned in the output pointers, false if the brush doesn't exist
+	virtual bool GetBrushInfo(int iBrush, CUtlVector<BrushSideInfo_t> *pBrushSideInfoOut, int *pContentsOut) = 0;
+
+	virtual bool PointOutsideWorld(const Vector &ptTest) = 0; //Tests a point to see if it's outside any playable area
+
+	// Walks bsp to find the leaf containing the specified point
+	virtual int GetLeafContainingPoint(const Vector &ptTest) = 0;
+
+	virtual ITraceListData *AllocTraceListData() = 0;
+	virtual void FreeTraceListData(ITraceListData *) = 0;
+
+	/// Used only in debugging: get/set/clear/increment the trace debug counter. See comment below for details.
+	virtual int GetSetDebugTraceCounter(int value, DebugTraceCounterBehavior_t behavior) = 0;
 };
