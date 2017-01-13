d6b4504d2450afabce1c8d0360ecd676373ebb57
aixxe <me@aixxe.net>
2017-01-13 19:22:31 +0000

    Properly fix unloading.
    * Fix braces style.
    
    Signed-off-by: aixxe <me@aixxe.net>

--- src/interfaces.cpp
+++ src/interfaces.cpp
@@ -25,7 +25,10 @@ void Interfaces::dumpInterfaces()
 		void* interfaces_sym = dlsym(library, "s_pInterfaceRegs");
 
 		if (!interfaces_sym)
+		{
+			dlclose(library);
 			continue;
+		}
 
 		dlclose(library);
 
