ba3cb9d2f8991fe8c1b71324bf8bf9b28d605a76
LuK1337 <priv.luk@gmail.com>
2017-01-29 20:44:35 +0100

    Call original function in DrawModelExecute instead of unhooking it

--- src/Hooks/DrawModelExecute.cpp
+++ src/Hooks/DrawModelExecute.cpp
@@ -2,20 +2,16 @@
 
 void Hooks::DrawModelExecute(void* thisptr, void* context, void *state, const ModelRenderInfo_t &pInfo, matrix3x4_t *pCustomBoneToWorld)
 {
-	modelRender_vmt->ReleaseVMT();
-
 	if (!Settings::ScreenshotCleaner::enabled || !engine->IsTakingScreenshot())
 	{
 		Chams::DrawModelExecute(thisptr, context, state, pInfo, pCustomBoneToWorld);
 	}
 
-	modelRender->DrawModelExecute(context, state, pInfo, pCustomBoneToWorld);
+	modelRender_vmt->GetOriginalMethod<DrawModelExecuteFn>(21)(thisptr, context, state, pInfo, pCustomBoneToWorld);
 	modelRender->ForcedMaterialOverride(NULL);
 
 	if (!Settings::ScreenshotCleaner::enabled || !engine->IsTakingScreenshot())
 	{
 		ESP::DrawModelExecute(thisptr, context, state, pInfo, pCustomBoneToWorld);
 	}
-
-	modelRender_vmt->ApplyVMT();
 }
