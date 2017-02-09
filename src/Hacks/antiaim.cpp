f86cf71304305985c590eaa5576ed2df068bd12f
Daniel Jones <McSwaggens@protonmail.com>
2017-02-10 00:40:57 +1100

    Revert "(Hopefully) Proper fake angles fix"
    This reverts commit 76a2cb454214d980722d5f31d6f1e639c72e280d.

--- src/Hacks/antiaim.cpp
+++ src/Hacks/antiaim.cpp
@@ -79,7 +79,7 @@ bool HasViableEnemy()
 
 void DoAntiAimY(QAngle& angle, int command_number, bool bFlip, bool& clamp)
 {
-	AntiAimType_Y aa_type = bFlip ? Settings::AntiAim::Yaw::type : Settings::AntiAim::Yaw::typeFake;
+	AntiAimType_Y aa_type = bFlip ? Settings::AntiAim::Yaw::typeFake : Settings::AntiAim::Yaw::type;
 
 	static bool yFlip;
 	float temp;
