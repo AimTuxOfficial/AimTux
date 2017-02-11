20273cb068e003cbe1fddccb6328b3adb3900204
certmemer <verynice1@memeware.net>
2017-02-11 15:06:50 +0100

    Remove useless materials from nosmoke.cpp (#683)
    * Update nosmoke.cpp
    
    * Update nosmoke.cpp

--- src/Hacks/nosmoke.cpp
+++ src/Hacks/nosmoke.cpp
@@ -2,33 +2,11 @@
 
 bool Settings::NoSmoke::enabled = false;
 
-// FIXME: i'm pretty sure we don't need all of these...
 std::vector<const char*> smoke_materials = {
-		"effects/overlaysmoke",
-		"particle/beam_smoke_01",
-		"particle/particle_smokegrenade",
-		"particle/particle_smokegrenade1",
-		"particle/particle_smokegrenade2",
-		"particle/particle_smokegrenade3",
-		"particle/particle_smokegrenade_sc",
-		"particle/smoke1/smoke1",
-		"particle/smoke1/smoke1_ash",
-		"particle/smoke1/smoke1_nearcull",
-		"particle/smoke1/smoke1_nearcull2",
-		"particle/smoke1/smoke1_snow",
-		"particle/smokesprites_0001",
-		"particle/smokestack",
-		"particle/vistasmokev1/vistasmokev1",
+		"particle/vistasmokev1/vistasmokev1_fire",
+		"particle/vistasmokev1/vistasmokev1_smokegrenade",
 		"particle/vistasmokev1/vistasmokev1_emods",
 		"particle/vistasmokev1/vistasmokev1_emods_impactdust",
-		"particle/vistasmokev1/vistasmokev1_fire",
-		"particle/vistasmokev1/vistasmokev1_nearcull",
-		"particle/vistasmokev1/vistasmokev1_nearcull_fog",
-		"particle/vistasmokev1/vistasmokev1_nearcull_nodepth",
-		"particle/vistasmokev1/vistasmokev1_smokegrenade",
-		"particle/vistasmokev1/vistasmokev4_emods_nocull",
-		"particle/vistasmokev1/vistasmokev4_nearcull",
-		"particle/vistasmokev1/vistasmokev4_nocull"
 };
 
 bool NoSmoke::RenderSmokePostViewmodel()
