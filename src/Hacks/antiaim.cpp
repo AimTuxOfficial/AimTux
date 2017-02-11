4edf7030ed6ecbd71891b6757cec203247e21fba
machineTheGlobal <decibelbills@gmail.com>
2017-02-11 23:49:32 +0000

    Add files via upload

--- src/Hacks/antiaim.cpp
+++ src/Hacks/antiaim.cpp
@@ -184,6 +184,16 @@ void DoAntiAimY(QAngle& angle, int command_number, bool bFlip, bool& clamp)
 			clamp = false;
 			factor = (globalVars->curtime * 5000.0f);
 			angle.y = factor + 36000000.0f;
+		case AntiAimType_Y::JITTER_180:
+			clamp = false;
+			yFlip ? angle.y = 0.1f : angle.y = 179.0f;
+			factor = (globalVars->curtime * 9999.0f);
+			break;
+		case AntiAimType_Y::BACK_JITTER:
+			angle.x = 89.0f;
+			clamp = true;
+			yFlip ? angle.y = 200.0f : angle.y = 160.0f;
+			factor = (globalVars->curtime * 9999999999.0f);			
 			break;
 		default:
 			angle.y -= 0.0f;
