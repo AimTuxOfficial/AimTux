3d94a147b932f256a89516a8f17da8b2b9782cfc
LuK1337 <priv.luk@gmail.com>
2017-02-11 01:53:45 +0100

    Random code style changes

--- src/Hacks/grenadehelper.cpp
+++ src/Hacks/grenadehelper.cpp
@@ -13,8 +13,8 @@ ImColor Settings::GrenadeHelper::infoSmoke = ImColor(30, 30, 30, 255);
 ImColor Settings::GrenadeHelper::infoFlash = ImColor(255, 255, 0, 255);
 
 std::vector<GrenadeHelper::GrenadeInfo> /**Settings::GrenadeHelper::**/grenadeInfos = {
-		GrenadeHelper::GrenadeInfo(GrenadeHelper::GrenadeType::SMOKE, Vector(1149.4f,-1183.97f,-141.51f), QAngle(-42.f,-165.f,0.f), GrenadeHelper::ThrowType::NORMAL),
-		GrenadeHelper::GrenadeInfo(GrenadeHelper::GrenadeType::FLASH, Vector(803,-1418,-44.91f), QAngle(-30.49f,-178.85f,0.f), GrenadeHelper::ThrowType::NORMAL)
+		GrenadeHelper::GrenadeInfo(GrenadeHelper::GrenadeType::SMOKE, Vector(1149.4f, -1183.97f, -141.51f), QAngle(-42.f, -165.f, 0.f), GrenadeHelper::ThrowType::NORMAL),
+		GrenadeHelper::GrenadeInfo(GrenadeHelper::GrenadeType::FLASH, Vector(803, -1418, -44.91f), QAngle(-30.49f, -178.85f, 0.f), GrenadeHelper::ThrowType::NORMAL)
 };
 
 bool GrenadeHelper::matches(C_BaseCombatWeapon* wpn, GrenadeHelper::GrenadeType type)
@@ -29,7 +29,7 @@ bool GrenadeHelper::matches(C_BaseCombatWeapon* wpn, GrenadeHelper::GrenadeType 
 			return strcmp(wpn->GetCSWpnData()->szClassName, "weapon_flashbang") == 0;
 		case GrenadeHelper::GrenadeType::MOLOTOV:
 			return strcmp(wpn->GetCSWpnData()->szClassName, "weapon_molotov") == 0
-					|| strcmp(wpn->GetCSWpnData()->szClassName, "weapon_incgrenade") == 0;
+				   || strcmp(wpn->GetCSWpnData()->szClassName, "weapon_incgrenade") == 0;
 		default:
 			return false;
 	}
@@ -47,6 +47,8 @@ ImColor getColor(GrenadeHelper::GrenadeType type)
 			return Settings::GrenadeHelper::infoFlash;
 		case GrenadeHelper::GrenadeType::MOLOTOV:
 			return Settings::GrenadeHelper::infoMolotov;
+		default:
+			return ImColor(255, 255, 255);
 	}
 }
 
@@ -55,6 +57,7 @@ void GrenadeHelper::DrawGrenadeInfo(GrenadeInfo* info)
 	Vector pos2d;
 	if (debugOverlay->ScreenPosition(Vector(info->pos.x, info->pos.y, info->pos.z), pos2d))
 		return;
+
 	Draw::Circle(Vector2D(pos2d.x, pos2d.y), 15, 20, Color::FromImColor(getColor(info->gType)));
 }
 
@@ -64,15 +67,21 @@ void GrenadeHelper::DrawAimHelp(GrenadeInfo* info)
 	Math::AngleVectors(info->angle, infoVec);
 	infoVec *= 150 / infoVec.Length();
 	Vector aim = info->pos + infoVec;
+
 	Vector posVec;
 	if (debugOverlay->ScreenPosition(aim, posVec))
 		return;
-	Vector2D pos2d(posVec.x, posVec.y);
-	Draw::FilledCircle(pos2d, 20, 5, Color::FromImColor(Settings::GrenadeHelper::aimDot));//Draw Point to Throw to
 
 	int w, h;
 	engine->GetScreenSize(w, h);
-	Draw::Line(Vector2D(w / 2, h / 2), pos2d, Color::FromImColor(Settings::GrenadeHelper::aimLine));//Draw Help line
+
+	Vector2D pos2d(posVec.x, posVec.y);
+
+	// Draw Point to Throw to
+	Draw::FilledCircle(pos2d, 20, 5, Color::FromImColor(Settings::GrenadeHelper::aimDot));
+
+	// Draw Help line
+	Draw::Line(Vector2D(w / 2, h / 2), pos2d, Color::FromImColor(Settings::GrenadeHelper::aimLine));
 }
 
 void GrenadeHelper::Paint()
@@ -83,30 +92,27 @@ void GrenadeHelper::Paint()
 	if (!engine->IsInGame())
 		return;
 
-	C_BasePlayer* localplayer = (C_BasePlayer*) entityList->GetClientEntity(engine->GetLocalPlayer());
-	if (!localplayer)
+	C_BasePlayer* localPlayer = (C_BasePlayer*) entityList->GetClientEntity(engine->GetLocalPlayer());
+	if (!localPlayer)
 		return;
 
-	C_BaseCombatWeapon* activeWeapon = (C_BaseCombatWeapon*) entityList->GetClientEntityFromHandle(localplayer->GetActiveWeapon());
-
+	C_BaseCombatWeapon* activeWeapon = (C_BaseCombatWeapon*) entityList->GetClientEntityFromHandle(localPlayer->GetActiveWeapon());
 	if (!activeWeapon || activeWeapon->GetCSWpnData()->GetWeaponType() != CSWeaponType::WEAPONTYPE_GRENADE)
 		return;
 
-	//draw the info
 	for (auto grenadeInfo = grenadeInfos.begin(); grenadeInfo != grenadeInfos.end(); grenadeInfo++)
 	{
 		if (Settings::GrenadeHelper::onlyMatchingInfos && !matches(activeWeapon,grenadeInfo->gType))
 			continue;
 
-		float dist = grenadeInfo->pos.DistTo(localplayer->GetVecOrigin());
+		float dist = grenadeInfo->pos.DistTo(localPlayer->GetVecOrigin());
 		if (dist > 10000)
 			continue;
+
 		GrenadeHelper::DrawGrenadeInfo(grenadeInfo.base());
 
 		if (dist < 75)
-		{
 			GrenadeHelper::DrawAimHelp(grenadeInfo.base());
-		}
 	}
 }
 
@@ -115,16 +121,17 @@ void GrenadeHelper::AimAssist(CUserCmd* cmd)
 	if (!Settings::GrenadeHelper::aimAssist || !engine->IsInGame())
 		return;
 
-	C_BasePlayer* localplayer = (C_BasePlayer*) entityList->GetClientEntity(engine->GetLocalPlayer());
-	if (!localplayer)
+	C_BasePlayer* localPlayer = (C_BasePlayer*) entityList->GetClientEntity(engine->GetLocalPlayer());
+	if (!localPlayer)
 		return;
 
-	C_BaseCombatWeapon* activeWeapon = (C_BaseCombatWeapon*) entityList->GetClientEntityFromHandle(localplayer->GetActiveWeapon());
+	C_BaseCombatWeapon* activeWeapon = (C_BaseCombatWeapon*) entityList->GetClientEntityFromHandle(localPlayer->GetActiveWeapon());
 
 	if (!activeWeapon || activeWeapon->GetCSWpnData()->GetWeaponType() != CSWeaponType::WEAPONTYPE_GRENADE)
 		return;
 
-	bool shootThisTick = cmd->buttons & IN_ATTACK;//If he is shooting, he will not throw the Grenade
+	// If he is shooting, he will not throw the Grenade
+	bool shootThisTick = cmd->buttons & IN_ATTACK;
 	if (!shootThisTick && !shotLastTick)
 		return;
 
@@ -132,23 +139,24 @@ void GrenadeHelper::AimAssist(CUserCmd* cmd)
 	{
 		if (Settings::GrenadeHelper::onlyMatchingInfos && !matches(activeWeapon,grenadeInfo->gType))
 			continue;
-		float dist = Math::GetDistance(localplayer->GetEyePosition(), grenadeInfo->pos);
+
+		float dist = Math::GetDistance(localPlayer->GetEyePosition(), grenadeInfo->pos);
 		if (dist > 75.f)
 			continue;
 
-		if (dist> 3.f)
+		if (dist > 3.f)
 		{
-			QAngle n = Math::CalcAngle(localplayer->GetEyePosition(),grenadeInfo->pos);
+			QAngle n = Math::CalcAngle(localPlayer->GetEyePosition(),grenadeInfo->pos);
 			n.x = cmd->viewangles.x;
 			cmd->viewangles = n;
 			cmd->forwardmove = dist;
-			//Todo make it not change the viewangles. Pretty Ghetto atm
+			// Todo make it not change the viewangles. Pretty Ghetto atm
 		}
 		else if (!shootThisTick && shotLastTick)
 		{
 			engine->SetViewAngles(grenadeInfo->angle);
-			//cmd->viewangles = grenadeInfo->angle;
-			//TODO Not working ;( maybe safe the value and Do it later or dont do it silent.
+			// cmd->viewangles = grenadeInfo->angle;
+			// TODO Not working ;( maybe safe the value and Do it later or dont do it silent.
 		}
 		break;
 	}
