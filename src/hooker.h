e11c1e6f5f27cb6f77bb88729ad10f69c2b1144e
Travis Lee <Shusako@users.noreply.github.com>
2017-02-14 21:31:18 -0500

    Fixed the sig for the update (#696)
    Updated sig

--- src/hooker.h
+++ src/hooker.h
@@ -60,7 +60,7 @@
 #define CAMTHINK_SVCHEATSCHECK_SIGNATURE "\x74\x00\x49\x83\x00\x00\x00\x00\x00\x00\x00\x0F\x84\x00\x00\x00\x00\x49\x8B"
 #define CAMTHINK_SVCHEATSCHECK_MASK "x?xx??????xxx????xx"
 
-#define CGAMESERVER_SIGNATURE "\x48\x8B\x1D\x38\x92\xB8\x00\x49\x89\xC6"
+#define CGAMESERVER_SIGNATURE "\x48\x8B\x1D\x48\x9B\xB8\x00\x49\x89\xC6"
 #define CGAMESERVER_MASK "xxxxxx?xxx"
 
 #include <memory>
