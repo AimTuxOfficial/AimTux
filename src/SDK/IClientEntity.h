c9fa834233f4320eaccb5eff8164babb83d5d5c5
Kurt Cancemi <kurt@x64architecture.com>
2017-03-24 23:48:10 -0400

    Update CCSWeaponInfo offsets

--- src/SDK/IClientEntity.h
+++ src/SDK/IClientEntity.h
@@ -538,57 +538,57 @@ class CCSWeaponInfo : public FileWeaponInfo_t
 public:
 	CSWeaponType GetWeaponType()
 	{
-		return *(CSWeaponType*)((uintptr_t)this + 0x814);
+		return *(CSWeaponType*)((uintptr_t)this + 0x864);
 	}
 
 	bool IsFullAuto()
 	{
-		return *(bool*)((uintptr_t)this + 0x820);
+		return *(bool*)((uintptr_t)this + 0x870);
 	}
 
 	float GetWeaponArmorRatio()
 	{
-		return *(float*)((uintptr_t)this + 0x82C);
+		return *(float*)((uintptr_t)this + 0x87C);
 	}
 
 	float GetMaxPlayerSpeed()
 	{
-		return *(float*)((uintptr_t)this + 0x830);
+		return *(float*)((uintptr_t)this + 0x880);
 	}
 
 	float GetMaxPlayerSpeedAlt()
 	{
-		return *(float*)((uintptr_t)this + 0x834);
+		return *(float*)((uintptr_t)this + 0x884);
 	}
 
 	float GetPenetration()
 	{
-		return *(float*)((uintptr_t)this + 0x840);
+		return *(float*)((uintptr_t)this + 0x890);
 	}
 
 	int GetDamage()
 	{
-		return *(int*)((uintptr_t)this + 0x844);
+		return *(int*)((uintptr_t)this + 0x894);
 	}
 
 	float GetRange()
 	{
-		return *(float*)((uintptr_t)this + 0x848);
+		return *(float*)((uintptr_t)this + 0x898);
 	}
 
 	float GetRangeModifier()
 	{
-		return *(float*)((uintptr_t)this + 0x84C);
+		return *(float*)((uintptr_t)this + 0x89C);
 	}
 
 	float GetSpread()
 	{
-		return *(float*)((uintptr_t)this + 0x9FC);
+		return *(float*)((uintptr_t)this + 0xA4C);
 	}
 
 	int GetZoomLevels()
 	{
-		return *(int*)((uintptr_t)this + 0xE88);
+		return *(int*)((uintptr_t)this + 0xEE0);
 	}
 };
 
