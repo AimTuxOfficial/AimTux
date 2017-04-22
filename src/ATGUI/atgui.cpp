77252aa7d7dcae78e2440e7242c3589ddf5dc444
Wando1423 <kamil531@gmail.com>
2017-04-22 12:35:32 +0200

    Changed name

--- src/ATGUI/atgui.cpp
+++ src/ATGUI/atgui.cpp
@@ -49,7 +49,7 @@ void UI::SwapWindow()
 	if (engine->IsInGame())
 		return;
 
-	Draw::ImDrawText(ImVec2(4.f, 4.f), ImColor(255, 255, 255, 255), "AimTux", NULL, 0.0f, NULL, ImFontFlags_Shadow);
+	Draw::ImDrawText(ImVec2(4.f, 4.f), ImColor(255, 255, 255, 255), "Wando's AimTux", NULL, 0.0f, NULL, ImFontFlags_Shadow);
 }
 
 void UI::SetVisible(bool visible)
