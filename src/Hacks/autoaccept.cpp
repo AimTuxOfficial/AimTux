db7e976e0421b430647d559b34539b9cb82004d2
LWSS <lwss@localhost.localdomain>
2017-05-24 17:19:24 -0700

    Autoaccept new sound-name

--- src/Hacks/autoaccept.cpp
+++ src/Hacks/autoaccept.cpp
@@ -15,7 +15,7 @@ void AutoAccept::PlaySound(const char* filename)
 	if (engine->IsInGame())
 		return;
 
-	if (strcmp(filename, "weapons/hegrenade/beep.wav") != 0)
+	if (strcmp(filename, "UI/competitive_accept_beep.wav") != 0) // new wav file on autoaccept, thanks @flawww
 		return;
 
 	CServerConfirmedReservationCheckCallback empty_callback;
