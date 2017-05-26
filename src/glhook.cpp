60cad635fd9ca4ea7d26a0b657c9caca92030bbc
Jai Mu <kwibjo@hotmail.com.au>
2017-05-27 02:32:22 +0930

    (Goof) Forgot to change glhook to use new font

--- src/glhook.cpp
+++ src/glhook.cpp
@@ -13,8 +13,21 @@ void SDL2::SwapWindow(SDL_Window* window)
 		fuzion_context = SDL_GL_CreateContext(window);
 		ImGui_ImplSdl_Init(window);
 
+		/*
 		ImWchar RobotoMonoRegular_ranges[] = {
 				0x0020, 0x007F, // Basic Latin
+				0x00A0, 0x00FF, // Latin-1 Supplement
+				0x0100, 0x017F, // Latin Extended-A
+				0x0180, 0x024F, // Latin Extended-B
+				0x0370, 0x03FF, // Greek and Coptic
+				0x0400, 0x04FF, // Cyrillic
+				0x0500, 0x052F, // Cyrillic Supplementary
+				0
+		};
+		*/
+
+		ImWchar SegoeUI_ranges[] = {
+				0x0020, 0x007E, // Basic Latin
 				0x00A0, 0x00FF, // Latin-1 Supplement
 				0x0100, 0x017F, // Latin Extended-A
 				0x0180, 0x024F, // Latin Extended-B
@@ -36,7 +49,7 @@ void SDL2::SwapWindow(SDL_Window* window)
 		ImFontConfig config;
 
 		// Add Roboto as default font
-		io.Fonts->AddFontFromMemoryCompressedTTF(RobotoMonoRegular_compressed_data, RobotoMonoRegular_compressed_size, 18.0f, &config, RobotoMonoRegular_ranges);
+		io.Fonts->AddFontFromMemoryCompressedTTF(SegoeUI_compressed_data, SegoeUI_compressed_size, 18.0f, &config, SegoeUI_ranges);
 
 		// Enable MergeMode and add additional fonts
 		config.MergeMode = true;
@@ -90,9 +103,9 @@ void SDL2::UnhookWindow()
 int SDL2::PollEvent(SDL_Event* event)
 {
 	static SDL_PollEvent_t oSDL_PollEvent = reinterpret_cast<SDL_PollEvent_t>(oPollEvent);
-	
+
 	Shortcuts::PollEvent(event);
-	
+
 	return oSDL_PollEvent(event);
 }
 
--- src/glhook.h
+++ src/glhook.h
@@ -8,7 +8,9 @@
 #include "ImGUI/imgui_impl_sdl.h"
 #include "ImGUI/imgui_internal.h"
 #include "ImGUI/fonts/KaiGenGothicCNRegular.h"
-#include "ImGUI/fonts/RobotoMonoRegular.h"
+//#include "ImGUI/fonts/RobotoMonoRegular.h"
+#include "ATGUI/SegoeUI.h"
+
 
 typedef void (*SDL_GL_SwapWindow_t) (SDL_Window*);
 typedef int (*SDL_PollEvent_t) (SDL_Event*);
