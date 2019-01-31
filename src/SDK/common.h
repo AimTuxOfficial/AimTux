84f9406faa91250891ec753f9984eaf1f7a4e3f5
LWSS <bbchallenger100@gmail.com>
2019-01-31 08:39:08 -0800

    Clang fix

--- src/SDK/common.h
+++ src/SDK/common.h
@@ -18,15 +18,9 @@ inline void**& getvtable(void* inst, size_t offset = 0)
 	return *reinterpret_cast<void***>((size_t)inst + offset);
 }
 
-inline const void** getvtable(const void* inst, size_t offset = 0)
-{
-	return *reinterpret_cast<const void***>((size_t)inst + offset);
-}
-
-template<typename Fn>
-inline Fn getvfunc(const void* inst, size_t index, size_t offset = 0)
-{
-	return reinterpret_cast<Fn>(getvtable(inst, offset)[index]);
+template< typename Fn >
+inline Fn getvfunc( void* inst, size_t index, size_t offset = 0 ) {
+    return reinterpret_cast<Fn>(getvtable( inst, offset )[index]);
 }
 
 template <typename interface>
